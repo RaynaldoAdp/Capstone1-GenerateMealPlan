36dc16a999cb6efd1e249540883f601d61e3b0fb
Mostafa <m.elsheikh11791@gmail.com>
2016-10-20 11:33:10 +0200

    minor refactors

--- app.js
+++ app.js
@@ -1,46 +1,43 @@
 //Global variables
 var bmrResult = 0;
-
 var recipeIndex = 1;
 
-
-//To Calculate Daily Calorie Intake
-function bmrCalculatorForMale(){
-  var age = parseInt($('#Age').val());
-  var height = parseInt($('#Height').val());
-  var weight = parseInt($('#Weight').val());
-  bmrResult = (66 + 13.7 * weight + 5 * height - 6.8 * age) * 1.5;
+function getEnteredFormData() {
+  return [parseInt($('#Age').val()), parseInt($('#Height').val()), parseInt($('#Weight').val())];
 }
 
-function bmrCalculatorForFemale(){
-  var age = parseInt($('#Age').val());
-  var height = parseInt($('#Height').val());
-  var weight = parseInt($('#Weight').val());
-  bmrResult = (655 + 9.6 * weight + 1.8 * height - 4.7 * age) *1.5;
+function bmrCalculator(male){
+  var formData = getEnteredFormData();
+  var age = formData[0];
+  var height = formData[1];
+  var weight = formData[2];
+  if (male) {
+    bmrResult = (66 + 13.7 * weight + 5 * height - 6.8 * age) * 1.5;
+  } else {
+    bmrResult = (655 + 9.6 * weight + 1.8 * height - 4.7 * age) *1.5;
+  }
 }
 
 function getGender(){
   if($('.Gender').val() === 'Male') {
-    bmrCalculatorForMale();
+    bmrCalculator(true);
   }
   else{
-    bmrCalculatorForFemale(bmrResult);
+    bmrCalculator(false);
   }
 }
 
 // Requesting from API + Callback
-
 var spoonacularUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/mealplans/generate';
 
-
-function getDataFromApi(caloriesPerDay, callback) {
+function getDataFromApi(callback) {
   var settings = {
     url: spoonacularUrl,
     beforeSend: function(xhr) {
       xhr.setRequestHeader('X-Mashape-Key', 'k5WAlgf0pnmshreZ5sTP5BMWeU90p1gm2eVjsnUAfcgsE5MzCZ');
     },
     data: {
-      targetCalories: caloriesPerDay,
+      targetCalories: bmrResult,
       timeFrame: 'day',
     },
     dataType: 'json',
@@ -60,60 +57,45 @@ function displayData(data) {
                             '<h4>Total amount of fat: '+ data.nutrients.fat +' g</h4>'+
                             '<h4>Total amount of carbohydrates: '+ data.nutrients.carbohydrates +' g</h4>'+
                             '<h4>These are the meals (click on image to get full information about the meal)</h4><br>'+
-                          '</div>'                    
+                          '</div>'
   if (data.meals) {
+    var mealType;
     data.meals.forEach(function(item, index) {
-      if(index === 0){  
-        resultElement += '<div class ="recipes col-4">' +
-                           '<h3> Meal for Breakfast </h3>' +
-                           '<p>' + item.title + '</p>' + 
-                           '<a href=" https://spoonacular.com/recipes/'+ item.title + '-' + item.id + '"><img src="https://webknox.com/recipeImages/'+ item.id +'-556x370.jpg"></a>' +
-                          '</div>';             
-      }
-      else if(index === 1){  
-        resultElement += '<div class ="recipes col-4">' +
-                           '<h3> Meal for Lunch </h3>' +
-                           '<p>' + item.title + '</p>' + 
-                           '<a href=" https://spoonacular.com/recipes/'+ item.title + '-' + item.id + '"><img src="https://webknox.com/recipeImages/'+ item.id +'-556x370.jpg"></a>' +
-                         '</div>';
-      }
-      else{  
-        resultElement += '<div class ="recipes col-4">' +
-                           '<h3> Meal for Dinner </h3>' +
-                           '<p>' + item.title + '</p>' + 
-                           '<a href=" https://spoonacular.com/recipes/'+ item.title + '-' + item.id + '"><img src="https://webknox.com/recipeImages/'+ item.id +'-556x370.jpg"></a>' +
-                         '</div>';
-
-      }
+      mealType = index === 0 ? "Breakfast" : (index === 1 ? "Lunch" : "Dinner");
+      resultElement = '<div class ="recipes col-4">'
+      + '<h3> Meal for ' +  mealType + '</h3>'
+      + '<p>' + item.title + '</p>'
+      + '<a href=" https://spoonacular.com/recipes/'+ item.title + '-' + item.id + '"><img src="https://webknox.com/recipeImages/'+ item.id +'-556x370.jpg"></a>'
+      + '</div>';
     });
   }
   else {
     resultElement += '<p>No results</p>';
   }
-  resultElement +=    '<div class ="generateMore">'+
-                       '<p>Not Satisfied with these recipes?Fret not. Generate more recipes by clicking the button below!</p>' +
-                       '<input type="button" class="js-generateMoreButton generateMoreButton" value="Give me more!">' +
+  resultElement += '<div class ="generateMore">'+
+                      '<p>Not Satisfied with these recipes?Fret not. Generate more recipes by clicking the button below!</p>' +
+                      '<input type="button" class="js-generateMoreButton generateMoreButton" value="Give me more!">' +
                       '</div>' +
-                   '</div>' + 
+                   '</div>' +
                    '<footer> <a href="https://github.com/RaynaldoAdp"><img src="images/github.png"></a> </footer>'+
-                  '</div>'; 
+                  '</div>';
   $('.js-recipeContainer').append(resultElement);
   generateMoreRecipe();
   scrollToNewElement();
 }
 
 
-function renderRecipe(bmrResult) {
-    getDataFromApi(bmrResult, displayData);
+function renderRecipe() {
+    getDataFromApi(displayData);
   }
 
-//function to generate more recipe 
+//function to generate more recipe
 
 function generateMoreRecipe() {
     $('.js-generateMoreButton').click(function(){
         $(this).closest("div").addClass('hidden');
         recipeIndex += 1;
-        renderRecipe(bmrResult);
+        renderRecipe();
     })
 }
 
@@ -130,6 +112,6 @@ $(document).ready(function(){
   $('.bmrCalculatorForm').submit(function(){
     event.preventDefault();
     getGender();
-    renderRecipe(bmrResult);
+    renderRecipe();
   })
 })
